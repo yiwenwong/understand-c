7fbfac4b5dfe46f27d1b84148d1304832bd8df03
banyuan <banyuan@wangyiwendeMacBook-Air.local>
2020-01-15 08:25:28 +0800

    third

---
+++ 绝对值.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int a;
+     
+
+    puts("请输入一个整数。");
+      scanf("%d", &a);
+    printf("它的绝对值是%d\n",fabs(a));
+     
+    return 0;
+}
